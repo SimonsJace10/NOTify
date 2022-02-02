8782411aaa71ecf1a6a104b41c9004dfda527f95
SimonsJace10 <Simons.Jace10@svvsd.org>
2022-02-02 10:55:13 -0700

    spotify api model initial commit

--- NOTify.py
+++ NOTify.py
@@ -3,18 +3,16 @@
 # Scrape banned FRC Competition songs from the spreadsheet and dump them into a spotify playlist using Selenium web auto
 # It is assumed that a spotify search will yield the correct song and not one with the same name.
 
-# selenium provides basic API access to do web automation tasks
-# in this project, it is used to interact with the spotify web player
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
 # initialize file_name to be DoNotPlayList.txt, which is assumed to be in scope of directory. This file is a stripped
 # down version of the original spreadsheet, but only contains the column of song names to avoid from parsing.
 file_name = "DoNotPlayList.txt"
 
 # the webdriver is a file published by selenium which allows for automation of a specific browser - in this case Chrome
-driver = webdriver.Chrome(r'chromedriver.exe')
+import requests
+import urllib.parse
+
+api_token = "BQD_TKuKFe1O8UTBGeTiutMqotHE6vAGgy-HEUoirIf8V6u4Eil_7n6Uib7PsGytVjg4tWOM-mjBxmn8taMwewIIKZzQ5SlnmFuSMWDN5zK" \
+            "l6S0oeyKuOpKJMDT81j4Oy2PS8_uELGbgAnXdPWPiKXqgaWe-PtjS7pArZHE6KFX02OFN4mE"
 
 
 def open_file(file_name):
@@ -32,16 +30,30 @@ def open_file(file_name):
         exit()
 
 
-def add_song_to_playlist(song_name):
+def add_song_to_playlist(search_string):
     # take in a song name and search for it on the Spotify web player
     # then, add it to the playlist
+    query = urllib.parse.quote(search_string)
+    url = f"https://api.spotify.com/v1/search?q={query}&type=track"
+    response = requests.get(
+        url,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_token}"
+        }
+    )
+    response_json = response.json()
 
-    driver.get('https://open.spotify.com/search')
-    driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[1]/header/div[3]/div/div/form/input').\
-        context_click()
+    results = response_json['tracks']['items']
+    if results:
+        return results[0]['id']
+    else:
+        raise Exception(f"{search_string} yielded no results.")
 
 
-def fetch_and_search_song_name():
+
+
+def fetch_song_name():
     count = 0
     # go line by line through the file from open_file
     for line in open_file(file_name):
@@ -50,14 +62,4 @@ def fetch_and_search_song_name():
         count += 1
 
 
-def login():
-    driver.get('https://open.spotify.com/')
-    driver.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/div[1]/header/div[5]/button[2]').click()
-    driver.find_element(By.CSS_SELECTOR, '#login-username').click().send_keys('stickyscissors15')
-    driver.find_element(By.CSS_SELECTOR, '#login-password').click.send_keys('DummyPass1234')
-    driver.find_element(By.XPATH, '//*[@id="login-button"]/div[1]').click()
-
-
-login()
-#fetch_and_search_song_name()
-#driver.quit()
+fetch_song_name()
