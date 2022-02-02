454b57ca1b1af0cc07d34bc1703375f206a77338
SimonsJace10 <Simons.Jace10@svvsd.org>
2022-02-02 14:13:11 -0700

    working but oauth token expire problem persists & cannot get valid playlist id

--- NOTify.py
+++ NOTify.py
@@ -3,18 +3,21 @@
 # Scrape banned FRC Competition songs from the spreadsheet and dump them into a spotify playlist using Selenium web auto
 # It is assumed that a spotify search will yield the correct song and not one with the same name.
 
+# the os allows access to env vars
+import os
+
+# requests and urllib are for interaction with spotify api
+import requests
+
+# set the api token to the env var
+api_token = os.getenv('SPOTIFY_AUTH_TOKEN')
+
 # initialize file_name to be DoNotPlayList.txt, which is assumed to be in scope of directory. This file is a stripped
 # down version of the original spreadsheet, but only contains the column of song names to avoid from parsing.
 file_name = "DoNotPlayList.txt"
 
-# the webdriver is a file published by selenium which allows for automation of a specific browser - in this case Chrome
-import requests
-import urllib.parse
 
-api_token = "BQD_TKuKFe1O8UTBGeTiutMqotHE6vAGgy-HEUoirIf8V6u4Eil_7n6Uib7PsGytVjg4tWOM-mjBxmn8taMwewIIKZzQ5SlnmFuSMWDN5zK" \
-            "l6S0oeyKuOpKJMDT81j4Oy2PS8_uELGbgAnXdPWPiKXqgaWe-PtjS7pArZHE6KFX02OFN4mE"
-
-
+# open_file handles opening the list of banned songs (file updated as of 2/1/22)
 def open_file(file_name):
     # try ...
     try:
@@ -30,36 +33,79 @@ def open_file(file_name):
         exit()
 
 
-def add_song_to_playlist(search_string):
-    # take in a song name and search for it on the Spotify web player
-    # then, add it to the playlist
-    query = urllib.parse.quote(search_string)
-    url = f"https://api.spotify.com/v1/search?q={query}&type=track"
+# search_for_song takes in the line (which contains the name of the artist + the song title) and search for it on the
+# Spotify web api
+def search_for_song(search_string):
+    # set the working url to the search api, and inject the query
+    # result_type = ["track"]
+    query = f"https://api.spotify.com/v1/search?query=track%3A{search_string}+artist%3A&type=track&offset=0&limit=20"
+    # query = f"https://api.spotify.com/v1/search?query=track%3A{search_string}+&type={result_type}&offset=0&limit=20"
+    # # response is an object to interact with the spotify web API
     response = requests.get(
-        url,
+        query,
+        # headers are required by the spotify API and are injected as json
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {}".format(api_token)
+        }
+    )
+    # instantiate response_json object to store the data returned from our search
+    response_json = response.json()
+    print(response_json)
+    results = response_json["tracks"]["items"]
+
+    uri = results[0]["uri"]
+    return uri
+
+    # # store results by calling the response json from above with the key 'tracks' and value 'items' pair
+    # # results = response_json['tracks']['items']
+    # # if results is not empty ...
+    # if results:
+    #     # ... store the first result and return it
+    #     # this code assumes the first song is the correct one, given we are searching for it using the song name and the
+    #     # artist's name
+    #     return results[0]['id']
+    # # if results is empty
+    # else:
+    #     # throw exception
+    #     raise Exception(f"{search_string} yielded no results.")
+
+
+# function to add the song to the do not playlist, taking in the raw line from the file parser
+def add_song_to_playlist(raw_line):
+    # the playlist url is hardcoded because we are accessing a specific playlist
+    playlist_url = "https://api.spotify.com/v1/playlists/2q6kfPEBIefmk3BlhC93Ee/tracks"
+    # using the response object, put the song into the playlist
+    response = requests.put(
+        # playlist url is the working url
+        playlist_url,
+        # inject json
+        json={
+            # the song id is returned by searching for the song with the raw search term from the text file
+            "ids": [search_for_song(raw_line)]
+        },
+        # once again, the headers are required by the spotify api
         headers={
             "Content-Type": "application/json",
             "Authorization": f"Bearer {api_token}"
         }
     )
-    response_json = response.json()
-
-    results = response_json['tracks']['items']
-    if results:
-        return results[0]['id']
-    else:
-        raise Exception(f"{search_string} yielded no results.")
+    # return boolean of whether the response object contains something
+    return response.ok
 
 
-
-
-def fetch_song_name():
+# parse the text file to extract the artist name + song title
+def parse_text_file():
+    # counter to keep track of lines analyzed
     count = 0
-    # go line by line through the file from open_file
+    # go line by line through the file returned by open_file
     for line in open_file(file_name):
-        # take the song name and pass it to the add_song_to_playlist function
+        print(line)
+        # take the line (which consists of song title and artist name) and pass it to the add_song_to_playlist function
         add_song_to_playlist(line)
+        # increment counter
         count += 1
 
 
-fetch_song_name()
+# call the parse_text_file function to effectively start the program
+parse_text_file()
